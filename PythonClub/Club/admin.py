6b46cee379a0fbaf3ae7264c946538dfaac27fd9
MonaAtabanii <42449312+MonaAtabanii@users.noreply.github.com>
2019-04-18 13:16:55 -0700

    Add to the models.py + register on admin.py
    Add to the models.py + register on admin.py + makemigration + migrate + create superuser

--- PythonClub/Club/admin.py
+++ PythonClub/Club/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Meeting, MeetingMinutes, Resource, Event
 
-# Register your models here.
+# Register the models 
+admin.site.register(Meeting)
+admin.site.register(MeetingMinutes)
+admin.site.register(Resource)
+admin.site.register(Event)
--- PythonClub/Club/models.py
+++ PythonClub/Club/models.py
@@ -1,3 +1,63 @@
-from django.db import models
+from django.db import models 
+from django.contrib.auth.models import User
 
-# Create your models here.
+#Meeting
+class Meeting(models.Model):
+    meetingtitle=models.CharField(max_length=255)
+    meetingdate=models.DateField()
+    meetingtime=models.TimeField()
+    meetinglocation=models.TextField(null=True, blank=True)
+    meetingagenda=models.TextField(null=True, blank=True)
+
+    def _str_(self):
+        return self.meetingtitle
+    
+    class Meta:
+        db_table = 'meeting'
+
+
+
+#Meeting Minutes
+class MeetingMinutes(models.Model):
+    meetingid = models.ForeignKey(Meeting, on_delete=models.DO_NOTHING) 
+    attendance=models.ManyToManyField(User)  
+    minutestext=models.TextField(null=True, blank=True)
+
+    def _str_(self):
+        return self.minutestext
+
+    class Meta:
+        db_table = 'meetingminuts'
+            
+
+#Resource
+class Resource(models.Model):
+    resourcename=models.CharField(max_length=255)
+    resourcetype=models.CharField(max_length=255)
+    resourceurl=models.URLField(null=True, blank=True)
+    dateentered=models.DateField()
+    userid=models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    resourcedescription=models.TextField(null=True, blank=True)
+
+    def _str_(self):
+        return self.resourcename
+    
+    class Meta:
+        db_table='resource'
+
+
+#Event
+class Event(models.Model):
+    eventtitle=models.CharField(max_length=255)
+    eventlocation=models.TextField(null=True, blank=True)
+    eventdate=models.DateField()
+    eventtime=models.TimeField()
+    userid=models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    eventdescription=models.TextField(null=True, blank=True)
+    userid=models.ForeignKey(User, on_delete=models.DO_NOTHING)
+
+    def _str_(self):
+        return self.eventtitle
+    
+    class Meta:
+        db_table='event'
